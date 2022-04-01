f494c608d609fa0d7c10182f81656b91c9797378
Rochel B. Furton <92700400+4tonRochel@users.noreply.github.com>
2022-04-01 22:56:44 +0800

    pic

--- Sidebar Menu - Dark Light Mode/script.js
+++ Sidebar Menu - Dark Light Mode/script.js
@@ -1,7 +1,6 @@
 const body = document.querySelector('body'),
       sidebar = body.querySelector('nav'),
       toggle = body.querySelector(".toggle"),
-      searchBtn = body.querySelector(".search-box"),
       modeSwitch = body.querySelector(".toggle-switch"),
       modeText = body.querySelector(".mode-text");
 
